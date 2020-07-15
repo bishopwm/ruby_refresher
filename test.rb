31a94bb9a18c4854c3858f0d710737d7f9fdaeea
bishopwm <bishopwm@g.cofc.edu>
2020-07-15 13:09:41 -0400

    ruby practice syntax

--- test.rb
+++ test.rb
@@ -1,1 +1,30 @@
-# test file for ruby refreshers
+class Employee
+
+    attr_accessor :name, :designation, :salary
+  
+    @@count = 0
+  
+    def initialize(name, designation, salary)
+      @name = name
+      @designation = designation
+      @salary = salary
+      @@count += 1
+    end
+  
+    def self.count
+      @@count
+    end
+  
+    private
+  
+    def organization
+      "Hackers de Anonymous"  
+    end
+  
+    protected
+  
+    def owner
+      "We never mention his name!"
+    end
+  
+  end
